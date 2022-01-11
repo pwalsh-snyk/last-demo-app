6e87bc82d9e8f826975f52002ff66f7b4c41ab46
Amit Rubinstein <amitrubinstein@Amits-MacBook-Pro-2.local>
2022-01-11 09:47:48 -0500

    test

--- routes/recycles.js
+++ routes/recycles.js
@@ -16,6 +16,7 @@ exports.sequelizeVulnerabilityChallenge = () => (req, res) => {
   })
 }
 
+
 exports.blockRecycleItems = () => (req, res) => {
   const errMsg = { err: 'Sorry, this endpoint is not supported.' }
   return res.send(utils.queryResultToJson(errMsg))
